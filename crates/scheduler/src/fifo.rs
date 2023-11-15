b01fa65270e5c81d474718b703891f935a502085
FeiFei0827 <2035585231@qq.com>
2023-11-16 00:32:30 +0800

    task5, (my alloc panic didn't work out)

--- crates/scheduler/src/fifo.rs
+++ crates/scheduler/src/fifo.rs
@@ -1,7 +1,7 @@
-use alloc::sync::Arc;
+use alloc::{collections::VecDeque, sync::Arc};
 use core::ops::Deref;
 
-use linked_list::{Adapter, Links, List};
+use linked_list::{Adapter, Links};
 
 use crate::BaseScheduler;
 
@@ -55,14 +55,14 @@ impl<T> Deref for FifoTask<T> {
 ///
 /// It internally uses a linked list as the ready queue.
 pub struct FifoScheduler<T> {
-    ready_queue: List<Arc<FifoTask<T>>>,
+    ready_queue: VecDeque<Arc<FifoTask<T>>>,
 }
 
 impl<T> FifoScheduler<T> {
     /// Creates a new empty [`FifoScheduler`].
     pub const fn new() -> Self {
         Self {
-            ready_queue: List::new(),
+            ready_queue: VecDeque::new(),
         }
     }
     /// get the name of scheduler
@@ -79,9 +79,12 @@ impl<T> BaseScheduler for FifoScheduler<T> {
     fn add_task(&mut self, task: Self::SchedItem) {
         self.ready_queue.push_back(task);
     }
-
+//from rr
     fn remove_task(&mut self, task: &Self::SchedItem) -> Option<Self::SchedItem> {
-        unsafe { self.ready_queue.remove(task) }
+        self.ready_queue
+            .iter()
+            .position(|t| Arc::ptr_eq(t, task))
+            .and_then(|idx| self.ready_queue.remove(idx))
     }
 
     fn pick_next_task(&mut self) -> Option<Self::SchedItem> {
@@ -89,11 +92,15 @@ impl<T> BaseScheduler for FifoScheduler<T> {
     }
 
     fn put_prev_task(&mut self, prev: Self::SchedItem, _preempt: bool) {
-        self.ready_queue.push_back(prev);
+        if _preempt {
+            self.ready_queue.push_back(prev);
+        } else {
+            self.ready_queue.push_front(prev);
+        }
     }
 
     fn task_tick(&mut self, _current: &Self::SchedItem) -> bool {
-        false // no reschedule
+        true
     }
 
     fn set_priority(&mut self, _task: &Self::SchedItem, _prio: isize) -> bool {
