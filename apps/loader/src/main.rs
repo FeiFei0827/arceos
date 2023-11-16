7f2cfd764de2e4f926c9d20549e593c581d4742f
FeiFei0827 <2035585231@qq.com>
2023-11-16 18:49:29 +0800

    step 5

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,57 +1,67 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 #![feature(asm_const)]
+
 #[cfg(feature = "axstd")]
 use axstd::println;
-use core::panic::PanicInfo;
+
 const PLASH_START: usize = 0x22000000;
 
 const SYS_HELLO: usize = 1;
 const SYS_PUTCHAR: usize = 2;
-
 static mut ABI_TABLE: [usize; 16] = [0; 16];
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let load_start = PLASH_START as *const u8;
     let load_size = 32; // Dangerous!!! We need to get accurate size of apps.
-    
+
     println!("Load payload ...");
+
     let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
-    println!("load code {:#x}; address [{:?}]", bytes_to_usize(&load_code), load_code.as_ptr());
+
+    println!("load code {:?}; address [{:?}]", load_code, load_code.as_ptr());
+    // switch aspace from kernel to app
+    unsafe { init_app_page_table(); }
+    unsafe { switch_app_aspace(); }
+
     // app running aspace
     // SBI(0x80000000) -> App <- Kernel(0x80200000)
     // 0xffff_ffc0_0000_0000
-    const RUN_START: usize = 0xffff_ffc0_8010_0000;
-    let run_code = unsafe {
-        core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
-    };
+    // const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    const RUN_START: usize = 0x4010_0000;
+
+    let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size) };
+    //println!("----------------------");
     run_code.copy_from_slice(load_code);
-    println!("run code {:#x}; address [{:?}]", bytes_to_usize(&run_code), run_code.as_ptr());
+    println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
     println!("Load payload ok!");
+
     register_abi(SYS_HELLO, abi_hello as usize);
     register_abi(SYS_PUTCHAR, abi_putchar as usize);
 
     // execute app
-    unsafe { core::arch::asm!("
+    unsafe {core::arch::asm!("
         la      a7, {abi_table}
         li      t2, {run_start}
         jalr    t2
         j       .",
         run_start = const RUN_START,
         abi_table = sym ABI_TABLE,
-    )}
+        )
+}
 }
 
-
-///usize::from_be_bytes(code[..8].try_into().unwrap()
+#[inline]
 fn bytes_to_usize(bytes: &[u8]) -> usize {
     let val = &bytes[..8];
     usize::from_be_bytes(val.try_into().unwrap())
 }
 
 fn register_abi(num: usize, handle: usize) {
-    unsafe { ABI_TABLE[num] = handle; }
+    unsafe {
+        ABI_TABLE[num] = handle;
+    }
 }
 
 fn abi_hello() {
@@ -60,4 +70,31 @@ fn abi_hello() {
 
 fn abi_putchar(c: char) {
     println!("[ABI:Print] {c}");
+}
+
+//
+// App aspace
+//
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize - axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
 }
