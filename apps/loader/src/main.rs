35864dd4950fcc0cf9e9b56c198f1d4b10a44c9d
FeiFei0827 <2035585231@qq.com>
2023-11-16 16:43:50 +0800

    step 2

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
-
+#![feature(asm_const)]
 #[cfg(feature = "axstd")]
 use axstd::println;
 
@@ -8,13 +8,39 @@ const PLASH_START: usize = 0x22000000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
+    let load_start = PLASH_START as *const u8;
+    let load_size = 32; // Dangerous!!! We need to get accurate size of apps.
 
     println!("Load payload ...");
 
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-    println!("content: {:#x}", usize::from_be_bytes(code[..8].try_into().unwrap()));
+    let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
+    println!("load code {:#x}; address [{:?}]", bytes_to_usize(&load_code), load_code.as_ptr());
+
+    // app running aspace
+    // SBI(0x80000000) -> App <- Kernel(0x80200000)
+    // 0xffff_ffc0_0000_0000
+    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+
+    let run_code = unsafe {
+        core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
+    };
+    run_code.copy_from_slice(load_code);
+    println!("run code {:#x}; address [{:?}]", bytes_to_usize(&run_code), run_code.as_ptr());
 
     println!("Load payload ok!");
+
+    println!("Execute app ...");
+    // execute app
+    unsafe { core::arch::asm!("
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+        run_start = const RUN_START,
+    )}
+}
+
+///usize::from_be_bytes(code[..8].try_into().unwrap()
+fn bytes_to_usize(bytes: &[u8]) -> usize {
+    let val = &bytes[..8];
+    usize::from_be_bytes(val.try_into().unwrap())
 }
