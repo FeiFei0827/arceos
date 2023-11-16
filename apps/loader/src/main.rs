4994f3b58b7faeebf9b0e5c936b5e0dcfdc10515
FeiFei0827 <2035585231@qq.com>
2023-11-16 17:43:11 +0800

    step 4

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -3,7 +3,7 @@
 #![feature(asm_const)]
 #[cfg(feature = "axstd")]
 use axstd::println;
-
+use core::panic::PanicInfo;
 const PLASH_START: usize = 0x22000000;
 
 const SYS_HELLO: usize = 1;
@@ -15,47 +15,34 @@ static mut ABI_TABLE: [usize; 16] = [0; 16];
 fn main() {
     let load_start = PLASH_START as *const u8;
     let load_size = 32; // Dangerous!!! We need to get accurate size of apps.
-
+    
     println!("Load payload ...");
-
     let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
     println!("load code {:#x}; address [{:?}]", bytes_to_usize(&load_code), load_code.as_ptr());
-
     // app running aspace
     // SBI(0x80000000) -> App <- Kernel(0x80200000)
     // 0xffff_ffc0_0000_0000
     const RUN_START: usize = 0xffff_ffc0_8010_0000;
-
     let run_code = unsafe {
         core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
     };
     run_code.copy_from_slice(load_code);
     println!("run code {:#x}; address [{:?}]", bytes_to_usize(&run_code), run_code.as_ptr());
-
     println!("Load payload ok!");
     register_abi(SYS_HELLO, abi_hello as usize);
     register_abi(SYS_PUTCHAR, abi_putchar as usize);
 
-    println!("Execute app ...");
-    let arg0: u8 = b'A';
     // execute app
     unsafe { core::arch::asm!("
-        li      t0, {abi_num}
-        slli    t0, t0, 3
-        la      t1, {abi_table}
-        add     t1, t1, t0
-        ld      t1, (t1)
-        jalr    t1
+        la      a7, {abi_table}
         li      t2, {run_start}
         jalr    t2
         j       .",
         run_start = const RUN_START,
         abi_table = sym ABI_TABLE,
-        //abi_num = const SYS_HELLO,
-        abi_num = const SYS_PUTCHAR,
-        in("a0") arg0,
     )}
 }
+
 
 ///usize::from_be_bytes(code[..8].try_into().unwrap()
 fn bytes_to_usize(bytes: &[u8]) -> usize {
